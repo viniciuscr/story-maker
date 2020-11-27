8c99930633ee76f5c1746c560b8c38e13c45ee19
Vinicius Reck <vinicius.reck@avenuecode.com>
2020-11-27 18:43:02 -0300

    feat: add fullscene scene

---
+++ src/stages/FullScene/FullScene.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TextBox from "../../features/textBox/TextBox";
+import structuralStyles from "./FullScene.module.css";
+import { selectScenery } from "../../features/scene/sceneSlice";
+import { useSelector } from "react-redux";
+
+const FullScene = () => {
+  const scenery = useSelector(selectScenery);
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${scenery})`,
+      }}
+      className={structuralStyles.scenario}
+    >
+      <div className={structuralStyles.scene}>
+        <TextBox translucent  />
+      </div>
+    </div>
+  );
+};
+
+export default FullScene;
