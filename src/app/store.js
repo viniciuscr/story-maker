b1c9365d859a6da23c87d3165a949d30c32ccae0
Vinícius CR <vinicius@DESKTOP-VPJ0RKI.localdomain>
2021-11-12 14:19:38 -0300

    feat: implementing some basic status update

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import sceneReducer from "../features/scene/sceneSlice";
 import dialogueReducer from "../features/dialogue/dialogueSlice";
+import statusReducer from "../features/status/statusSlice";
 
 export default configureStore({
   reducer: {
     scene: sceneReducer,
     dialogue: dialogueReducer,
+    status: statusReducer,
   },
 });
--- src/features/dialogue/useDialogue.js
+++ src/features/dialogue/useDialogue.js
@@ -5,16 +5,25 @@ import {
   nextDialogue,
   selectCurrentDialogue,
 } from "./dialogueSlice";
+import useStatus from "../status/useStatus";
 
 const useDialogue = () => {
   const dispatch = useDispatch();
-
+  const [_, setStatus] = useStatus();
   const dialogue = useSelector(selectCurrentDialogue) ?? {};
 
   const nextInteraction = (next) =>
     next.scene ? dispatch(goToScene(next.scene)) : dispatch(nextDialogue());
 
-  const choose = ({ next }) => {
+  const choose = ({ next, triggers }) => {
+    if (triggers) {
+      triggers.forEach(({ effect, value }) => {
+        if (effect === "update_status") {
+          setStatus(value.value, value.path);
+        }
+      });
+    }
+
     next.scene
       ? dispatch(goToScene(next.scene))
       : dispatch(goToDialogue(next.dialogue));
